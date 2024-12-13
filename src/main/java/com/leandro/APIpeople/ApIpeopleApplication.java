7c4bc5e4ff84530291e0486f0dfd8a0c5a4f492f
Leandro De Souza <ls306877@gmail.com>
2024-12-12 21:34:22 -0300

    Iniciação do projeto de gerenciamento de pessoas/estruturação de pastas

---
+++ src/main/java/com/leandro/APIpeople/ApIpeopleApplication.java
@@ -0,0 +1,13 @@
+package com.leandro.APIpeople;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApIpeopleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApIpeopleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leandro/APIpeople/ApIpeopleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leandro.APIpeople;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApIpeopleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
